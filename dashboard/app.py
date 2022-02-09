eb844acc3711d9838149ea5b041593defc723df2
Briana Brown <brianabrown274@gmail.com>
2022-02-08 19:23:01 -0500

    integrate ML model with Flask/HTML

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,6 @@
 # Dependencies
 from flask import Flask, render_template, request
+import predicting
 
 # Set up Flask
 app = Flask(__name__)
@@ -14,7 +15,8 @@ def index():
 def results():
     form_data = request.form
     genres = request.form.getlist("genres")
-    return render_template("results.html", form_data=form_data, genres=genres)
+    prediction = predicting.predict(form_data)
+    return render_template("results.html", form_data=form_data, genres=genres, prediction=prediction)
 
 if __name__ == "__main__":
     app.run()
